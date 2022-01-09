ce67e04fcbe95cef082f1f23d522e40eca954cfa
Pierce <pierce@vulpes.dev>
2022-01-08 21:47:09 -0500

    only set credits for traitors

--- lua/autorun/server/cfc_ttt_reduce_credits_for_lowpop.lua
+++ lua/autorun/server/cfc_ttt_reduce_credits_for_lowpop.lua
@@ -2,6 +2,6 @@ hook.Add("TTTBeginRound", "CFC_ReduceTraitorCredits", function()
 	if player.GetCount() > 3 then return end
 	local credits = math.max(0, player.GetCount()-2)
 	for _, ply in pairs(player.GetAll()) do
-		ply:SetCredits(credits)
+		if ply:IsRole(ROLE_TRAITOR) then ply:SetCredits(credits) end
 	end
 end)
